e024d65dc8428df5050fbf74bcabd453174e5077
Jeremiah Megel <git@jeremiahm.com>
2016-10-05 21:48:17 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"regexp"
+	"os/exec"
+)
+
+func main() {
+	resp, _ := http.Get("https://apod.nasa.gov/apod/astropix.html")
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	imgRegex, _ := regexp.Compile("<a href=\"(image/[^\"]+)\">")
+	imgPath := imgRegex.FindSubmatch(body)
+	imgResp, _ := http.Get("https://apod.nasa.gov/" + string(imgPath[1]))
+	img, _ := ioutil.ReadAll(imgResp.Body)
+	ioutil.WriteFile("/home/jm/abc.jpg", img, 0644)
+	cmd := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file:///home/jm/abc.jpg")
+	cmd.Start()
+}
+
