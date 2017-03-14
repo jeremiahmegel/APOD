892212daf15a1dd488d33e6248b6fc2cbaf2c59b
Jeremiah Megel <git@jeremiahm.com>
2017-03-13 20:20:37 -0500

    Move hardcoded URLs to constant strings

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"regexp"
 )
 
+const urlBase = "https://apod.nasa.gov/apod/"
+const htmlPath = "astropix.html"
+
 func downloadImage(imageURL string, fileLocation string) {
 	imgResp, _ := http.Get(imageURL)
 	defer imgResp.Body.Close()
@@ -15,12 +18,12 @@ func downloadImage(imageURL string, fileLocation string) {
 }
 
 func main() {
-	resp, _ := http.Get("https://apod.nasa.gov/apod/astropix.html")
+	resp, _ := http.Get(urlBase + htmlPath)
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	imgRegex, _ := regexp.Compile("<a href=\"(image/[^\"]+)\"")
 	imgPath := imgRegex.FindSubmatch(body)
-	downloadImage("https://apod.nasa.gov/" + string(imgPath[1]), "/home/jm/Pictures/Wallpapers/apod.jpg")
+	downloadImage(urlBase + string(imgPath[1]), "/home/jm/Pictures/Wallpapers/apod.jpg")
 	cmd := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file:///home/jm/Pictures/Wallpapers/apod.jpg")
 	cmd.Start()
 }
