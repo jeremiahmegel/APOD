dd043559ce9b9d2a26db712976318619d51ce8d0
Jeremiah Megel <git@jeremiahm.com>
2017-03-13 20:08:09 -0500

    Move image downloading to `downloadImage` function

--- main.go
+++ main.go
@@ -7,15 +7,20 @@ import (
 	"regexp"
 )
 
+func downloadImage(imageURL string, fileLocation string) {
+	print(imageURL, fileLocation)
+	imgResp, _ := http.Get(imageURL)
+	img, _ := ioutil.ReadAll(imgResp.Body)
+	ioutil.WriteFile(fileLocation, img, 0644)
+}
+
 func main() {
 	resp, _ := http.Get("https://apod.nasa.gov/apod/astropix.html")
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	imgRegex, _ := regexp.Compile("<a href=\"(image/[^\"]+)\"")
 	imgPath := imgRegex.FindSubmatch(body)
-	imgResp, _ := http.Get("https://apod.nasa.gov/" + string(imgPath[1]))
-	img, _ := ioutil.ReadAll(imgResp.Body)
-	ioutil.WriteFile("/home/jm/Pictures/Wallpapers/apod.jpg", img, 0644)
+	downloadImage("https://apod.nasa.gov/" + string(imgPath[1]), "/home/jm/Pictures/Wallpapers/apod.jpg")
 	cmd := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file:///home/jm/Pictures/Wallpapers/apod.jpg")
 	cmd.Start()
 }
